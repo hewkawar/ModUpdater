2e8ce92c028b9b1fe869eafd60ce0fd7224e4aad
HewkawAr <earthwangsaeng2@gmail.com>
2024-03-27 14:03:56 +0700

    Initial commit

---
+++ src/main/java/xyz/hewkawar/ModUpdater.java
@@ -0,0 +1,17 @@
+package xyz.hewkawar;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModUpdater implements ModInitializer {
+	public static final String MOD_ID = "mod-updater";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/xyz/hewkawar/ModUpdaterClient.java
@@ -0,0 +1,10 @@
+package xyz.hewkawar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModUpdaterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
